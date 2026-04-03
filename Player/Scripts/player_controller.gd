97752d61cec04cff2cd58942bce67ec436c5af3f
notadoctortired <alyxpeacockemail@gmail.com>
2026-04-03 19:47:28 +0100

    Added player health and damage function, added camera rotation by rotating player body by 45degrees per input

--- Player/Scripts/player_controller.gd
+++ Player/Scripts/player_controller.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-
+@export_category("Player Values")
+@export var health: float
+@export var speed: float
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -12,10 +12,21 @@ func _physics_process(delta: float) -> void:
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
 	move_and_slide()
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("rotate_right"):
+		# 0.7853982 is 45 degrees in radians
+		rotate_y(0.7853982)
+	
+	if event.is_action_pressed("rotate_left"):
+		rotate_y(-0.7853982)
+
+func damage(damage_amount: float):
+	health -= damage_amount
