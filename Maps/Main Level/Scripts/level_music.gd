4191030961f8c2cff59572d6172b26e41144a39d
notadoctortired <alyxpeacockemail@gmail.com>
2026-04-14 00:26:25 +0100

    Emergency fix for web build - separated the two soundtracks into separate audio streams to work with web build

--- Maps/Main Level/Scripts/level_music.gd
+++ Maps/Main Level/Scripts/level_music.gd
@@ -3,30 +3,30 @@ extends AudioStreamPlayer
 @export_category("Values")
 @export var volume: float
 
-var sorcerer_track = load("res://Music/SFX_battle_theme_sorcerers.wav")
-
 var playback = 0
 @onready var player = get_tree().root.get_child(0).get_node("Player/PlayerBody")
+@onready var sorcerer_track = get_tree().root.get_child(0).get_node("LevelMusicSorcerer")
 
 func _ready():
+	print(name)
 	var samples = stream.get_length()*stream.mix_rate
 	
 	volume_db = volume
 	stream.loop_mode = 1
 	stream.set_loop_end(samples)
 	
-	play()
+	if name == "LevelMusic":
+		play()
 
 func _process(delta: float):
-	if playing:
+	if playing and name == "LevelMusic":
 		playback = get_playback_position()
+	elif sorcerer_track.playing:
+		playback = sorcerer_track.get_playback_position()
 	
-	if not playing:
-		play(playback)
-	
-	if player.health < 100 and stream != sorcerer_track:
-		stream = sorcerer_track
-		play(playback)
+	if player.health < 100 and playing and name == "LevelMusic":
+		stop()
+		sorcerer_track.play(playback)
 		
 		var samples = stream.get_length()*stream.mix_rate
 		
